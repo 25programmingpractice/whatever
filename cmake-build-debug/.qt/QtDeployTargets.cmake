51a07c7ecbca3eff2af6753a37ddfd13680e273e
LJM12914 <LJM12914@outlook.com>
2025-07-16 15:23:58 +0800

    Delete cmake-build-debug directory

--- cmake-build-debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_whatever_FILE /Users/yzhbradoodrrpurp/Desktop/whatever/cmake-build-debug/whatever.app/Contents/MacOS/whatever)
-set(__QT_DEPLOY_TARGET_whatever_TYPE EXECUTABLE)
